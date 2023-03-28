4861ce2d6e559dc5a7560614aa5c51c5596c2cfd
cpinot <cynthia.priscila.pino@gmail.com>
2023-03-28 17:24:16 -0300

    Se realiza el modulo Agregar Configuración y Editar configuracion

--- apps/configuraciones/formularios.py
+++ apps/configuraciones/formularios.py
@@ -28,3 +28,24 @@ class FormularioCasilla(forms.ModelForm):
            *self.fields,
            Submit('submit', 'Enviar', css_class='d-grid gap-2 col-2 mx-auto mt-2 text-light border botonnew')
         )
+
+#Formulario de Configuraciones
+
+class FormularioConfiguracion(forms.ModelForm):
+
+    class Meta:        
+        model = Configuracion
+        fields = ["casilla","tipo_servicio","id_usuario","id_tenant","id_app","id_key","api_key","protocolo"]
+
+        labels ={
+                    "casilla" : "Casilla de Correo",
+                    "tipo_servicio" : "Tipo de Servicio"
+                } 
+      
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            *self.fields,
+           Submit('submit', 'Enviar', css_class='d-grid gap-2 col-2 mx-auto mt-2 ')
+        )
