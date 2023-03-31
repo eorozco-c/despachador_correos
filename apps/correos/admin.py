35b04b1d8ce04348673f4be0209dac96dc0a685a
Ernesto Orozco <42546849+eorozco-c@users.noreply.github.com>
2023-03-31 16:40:56 -0300

    Se remueven modelos de admin

--- apps/correos/admin.py
+++ apps/correos/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from .models import Correo, Estado, Ejecutivo
+
 # Register your models here.
-admin.site.register(Correo)
-admin.site.register(Estado)
-admin.site.register(Ejecutivo)
