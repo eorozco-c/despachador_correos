367de734c0c685a0e3595391b59df4f668548121
cpinot <cynthia.priscila.pino@gmail.com>
2023-04-03 15:22:12 -0400

    Vista Nuevo Ejecutivo, Editar Ejecutivo ,Eliminar Ejecutivo, Listar solo Ejecutivos

--- apps/correos/urls.py
+++ apps/correos/urls.py
@@ -6,5 +6,9 @@ app_name = 'correos'
 
 urlpatterns = [
     path('', views.ListarCorreos.as_view(), name="correos"),
-    
+    path('ejecutivos', views.ListarEjecutivos.as_view(), name="ejecutivos"),
+    path('crear_ejecutivo',views.CrearEjecutivo.as_view(),name='crear_ejecutivo'),
+    path('editar_ejecutivo/<int:pk>',views.EditarEjecutivo.as_view(),name='editar_ejecutivo'),
+    path('predestroy/<int:pk>',views.predestroy,name='predestroy'),
+    path('destroy/<int:pk>',views.destroy,name='destroy'),
 ]
